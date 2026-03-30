4c4d370f4941299e8c557f17bc9629a13dcfab1b
andrespalacios-24 <andresfelipepalaciosviveros@gmail.com>
2026-03-30 10:26:13 -0500

    primer contacto con python

---
+++ hello.py
@@ -0,0 +1,12 @@
+# hola mundo 
+print ("hola python")
+"""con estas 3 comillas se pueden 
+escribir comentarios en varias 
+lineas """
+print (type ("hola")) #tipo "str" que es texto
+print (type(54)) #tipo "int" que es de numero entero
+print (type(3.2)) #tipo "float" numeros con decimales
+print (type(True)) #tipo "bool" que es verdadero o falso 
+""" para saber que tipo de dato es 
+se debe poner el print seguido del type.
+si es escritura poner en comillas"""
