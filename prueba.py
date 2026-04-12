0dcf2aebe83d3a249c3ce5ff7c79a3bd6c7f95a5
ANDRES PALACIOS <andresfelipepalaciosviveros@gmail.com>
2026-04-12 17:51:20 -0500

    se hacen los ejercicios de practica propia hasta el 9

--- prueba.py
+++ prueba.py
@@ -1,8 +1,34 @@
-contador= 0
-numero = 1 
-while numero <=50:
-    if numero %5== 0:
-        contador +=1
-    numero +=1
-print(contador)
+# Ejercicio 10
+# Tienes esta lista de intentos de login con su resultado:
+# intentos = [
+#     {"usuario": "ana", "exitoso": True},
+#     {"usuario": "luis", "exitoso": False},
+#     {"usuario": "ana", "exitoso": False},
+#     {"usuario": "pedro", "exitoso": True},
+#     {"usuario": "luis", "exitoso": False},
+#     {"usuario": "luis", "exitoso": False},
+# ]
+# Usa un bucle for para:
+# - Contar los intentos fallidos por usuario
+# - Imprimir los usuarios que tengan 2 o más intentos fallidos (posible ataque)
+# Resultado esperado: luis tiene 3 intentos fallidos — ALERTA
 
+intentos = [
+     {"usuario": "ana", "exitoso": True},
+     {"usuario": "luis", "exitoso": False},
+     {"usuario": "ana", "exitoso": False},
+     {"usuario": "pedro", "exitoso": True},
+     {"usuario": "luis", "exitoso": False},
+     {"usuario": "luis", "exitoso": False},
+        ]
+
+fallido=0
+aceptado=0
+for login in intentos:
+    if (login["exitoso"]) ==False:
+        fallido+=1
+
+print(fallido)
+    #else:
+       # aceptado+=1
+       
