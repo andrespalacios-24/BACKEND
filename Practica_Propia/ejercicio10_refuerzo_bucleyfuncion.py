92df39f4b5d407242a09f3101933e405988f19e0
ANDRES PALACIOS <andresfelipepalaciosviveros@gmail.com>
2026-04-18 13:48:59 -0500

    se realiza ultimo punto de ejercicio de refuerzo donde se integran for, función, diccionario, max(), round(), y f-string

--- Practica_Propia/ejercicio10_refuerzo_bucleyfuncion.py
+++ Practica_Propia/ejercicio10_refuerzo_bucleyfuncion.py
@@ -151,7 +151,21 @@ intentos_login("backend123")
 ventas = [120, 340, 89, 456, 230, 178, 95]
 
 def resumen_ventas(ventas):
-    pass  # reemplaza con tu lógica
+   total= 0                 #las listas en 0 deben queda dentro de la funcion pero
+   promedio= 0              # fuera del bucle en este caso for
+   maximo= 0
+   for venta in ventas:
+    total += venta
+   maximo= max(ventas) 
+   promedio= round (total / len(ventas),2) #le puse round para que lo redondeara a 2 decimales
+   return{
+    "total":total,
+    "promedio": promedio, 
+    "maximo": maximo
+   }
+resultado= resumen_ventas(ventas) #esto se hace para que se procese la lista y devuelva el 
+                                  # diccionario que tenemos dentro de la funcion (lo hace internamente)
+print(f"total de ventas:{resultado["total"]} | promedio de ventas:{resultado["promedio"]} | venta maxima:{resultado["maximo"]}")
 
 
 # ------------------------------------------------------------
