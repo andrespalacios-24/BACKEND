531c103a6e46a997059966f00eb37b665270a173
ANDRES PALACIOS <andresfelipepalaciosviveros@gmail.com>
2026-05-25 11:33:37 -0500

    SQL: ejercicios SELECT básico cineDB

---
+++ SQL/statements/02_select_ejercicios.sql
@@ -0,0 +1,23 @@
+1. Obtener el id, título y año de estreno de todas las películas.
+
+SELECT movie_id, title, release_year FROM cineDB.movies;
+
+2. Obtener todo el contenido de la tabla "countries".
+
+SELECT * FROM countries;
+
+3. Obtener el nombre y fecha de nacimiento todas las personas almacenada en la tabla "people".
+
+SELECT name, birth_date FROM cineDB.people;
+
+4. Obtener todos los géneros de películas disponibles en la tabla "genres".
+
+SELECT genre_name FROM cineDB.genres;
+
+5. Obtener el nombre, cantidad de espectadores y recaudación de todas las películas.
+
+SELECT title, total_viewers, total_revenue FROM cineDB.movies;
+
+6. Obtener el nombre de los estudios cinematográficos almacenados en la tabla "studios".
+
+SELECT studio_name FROM cineDB.studios;
