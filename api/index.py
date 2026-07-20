6394cf55c6bee4bb4c7cf6828ee93e20d5e0a212
ANDRES PALACIOS <andresfelipepalaciosviveros@gmail.com>
2026-07-20 00:29:22 -0500

    fix: configuracion definitiva y limpia para vercel

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) )
 
-from FASTAPI.main_biblioteca import app
+from main_biblioteca import app
